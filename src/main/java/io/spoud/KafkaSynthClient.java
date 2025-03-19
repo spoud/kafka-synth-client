6ef87986ac2520d8aa43347943230d8249658f52
Simon Hofer <simon.hofer@spoud.io>
2025-03-19 06:00:00 +0100

    feat: add dashboard and multi-dc simulation, moved and consolidated redundant docs

--- src/main/java/io/spoud/KafkaSynthClient.java
+++ src/main/java/io/spoud/KafkaSynthClient.java
@@ -13,6 +13,7 @@ import jakarta.enterprise.event.Observes;
 import jakarta.interceptor.Interceptor;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.errors.TopicExistsException;
 
@@ -29,7 +30,7 @@ public class KafkaSynthClient {
     private final int messagesPerSecond;
     private final AdminClient adminClient;
     private final SynthClientConfig config;
-    private AtomicBoolean waitForTopicCreated = new AtomicBoolean(true);
+    private final AtomicBoolean waitForTopicCreated = new AtomicBoolean(true);
 
     public KafkaSynthClient(
             SynthClientConfig config,
@@ -47,36 +48,69 @@ public class KafkaSynthClient {
     public void start(@Observes  @Priority(Interceptor.Priority.APPLICATION - 1)  StartupEvent event) {
         if (config.autoCreateTopic()) {
             Log.infof("Creating topic %s", config.topic());
-            this.adminClient.listTopics().names().whenComplete((topics, throwable) -> {
-                if (throwable != null) {
-                    throw new RuntimeException("Failed to list topics", throwable);
+            adminClient.describeCluster().nodes().whenComplete((nodes, t) -> {
+                Log.debugf("Nodes %s", nodes);
+                if (t != null) {
+                    throw new RuntimeException("Failed to get nodes", t);
                 }
-                if (!topics.contains(config.topic())) {
-                    adminClient.describeCluster().nodes().whenComplete((nodes, t) -> {
-                        if (t != null) {
-                            throw new RuntimeException("Failed to get nodes", t);
-                        }
-                        this.adminClient.createTopics(List.of(new NewTopic(config.topic(), nodes.size(), (short) nodes.size()).configs(
-                                Map.of(TopicConfig.RETENTION_MS_CONFIG, "3600000")
-                        ))).all().whenComplete((v, t1) -> {
-                            if (t1 != null) {
-                                // if there is auto.create concurrently happening, we might get a TopicExistsException
-                                if(t1 instanceof TopicExistsException) {
-                                    waitForTopicCreated.set(false);
-                                    Log.infof("Topic already exists %s", config.topic());
+                short replicationFactor = (short) Math.max(Math.min(config.topicReplicationFactor(), nodes.size()), 1);
+                Log.debugf("ReplicationFactor calculated %s", replicationFactor);
+                if(config.topicReplicationFactor() <= 0) {
+                    Log.infof("Replication factor set to %s assuming number of nodes %s", config.topicReplicationFactor(), nodes.size());
+                    replicationFactor = (short) nodes.size();
+                }
+                short finalReplicationFactor = replicationFactor;
+                this.adminClient.listTopics().names().whenComplete((topics, throwable) -> {
+                    if (throwable != null) {
+                        throw new RuntimeException("Failed to list topics", throwable);
+                    }
+                    if (!topics.contains(config.topic())) {
+
+                            int minInSyncReplicas = finalReplicationFactor -1;
+                            this.adminClient.createTopics(List.of(new NewTopic(config.topic(), nodes.size(), finalReplicationFactor).configs(
+                                    Map.of(TopicConfig.RETENTION_MS_CONFIG, "3600000", TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, String.valueOf(minInSyncReplicas))
+                            ))).all().whenComplete((v, t1) -> {
+                                if (t1 != null) {
+                                    // if there is auto.create concurrently happening, we might get a TopicExistsException
+                                    if(t1 instanceof TopicExistsException) {
+                                        waitForTopicCreated.set(false);
+                                        Log.infof("Topic already exists %s", config.topic());
+                                    } else {
+                                        throw new RuntimeException("Failed to create topic", t1);
+                                    }
                                 } else {
-                                    throw new RuntimeException("Failed to create topic", t1);
+                                    Log.infof("Topic created %s", config.topic());
+                                    waitForTopicCreated.set(false);
                                 }
-                            } else {
-                                Log.infof("Topic created %s", config.topic());
-                                waitForTopicCreated.set(false);
+                            });
+                    } else {
+                        waitForTopicCreated.set(false);
+                        Log.infof("Topic already exists %s", config.topic());
+                        // check replication factor and min in sync replicas config for topic
+                        adminClient.describeTopics(List.of(config.topic())).allTopicNames().whenComplete((v, t1) -> {
+                            if (t1 != null) {
+                                throw new RuntimeException("Failed to describe topic", t1);
+                            }
+                            var replicationFactorConfig = v.get(config.topic()).partitions().getFirst().replicas().size();
+                            if( replicationFactorConfig != finalReplicationFactor) {
+                                Log.errorf("Replication factor for topic %s is %s, expected %s", config.topic(), replicationFactorConfig, finalReplicationFactor);
+                            }
+
+                        });
+
+                        ConfigResource cr = new ConfigResource(ConfigResource.Type.TOPIC, config.topic());
+                        adminClient.describeConfigs(List.of(cr)).all().whenComplete((configEntries, t1) -> {
+                            if (t1 != null) {
+                                throw new RuntimeException("Failed to get topic config", t1);
+                            }
+                            var minInSyncReplicasConfig = configEntries.get(cr).get(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG);
+                            if(minInSyncReplicasConfig == null || Integer.parseInt(minInSyncReplicasConfig.value()) != finalReplicationFactor -1) {
+                                Log.errorf("Min in sync replicas for topic %s is %s, expected %s", config.topic(), minInSyncReplicasConfig, finalReplicationFactor -1);
                             }
                         });
-                    });
-                } else {
-                    waitForTopicCreated.set(false);
-                    Log.infof("Topic already exists %s", config.topic());
-                }
+
+                    }
+                });
             });
         } else {
             waitForTopicCreated.set(false);
